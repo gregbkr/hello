56ec166cd25633bc00f281831dd615f6b95db82a
Greg <gregbkr@outlook.com>
2020-02-14 15:59:29 +0100

    Fix prod 3.1

--- server.js
+++ server.js
@@ -12,11 +12,7 @@ var hostname = os.hostname();
 // App
 const app = express();
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-  res.send(`Hello world *DEV* v2.8 from server: ${hostname}`);
-=======
-  res.send(`Hello world !DEV! v3.0 from server: ${hostname}`);
->>>>>>> 99015999031e8756ac3e76776712a155849c5825
+  res.send(`Hello world *PROD* v3.1 from server: ${hostname}`);
 });
 
 app.listen(PORT, HOST);
